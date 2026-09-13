a1457a252e191df0b333a447a4b2aca98925f010
Ragnarok93 <e_fontenot1@yahoo.com>
2026-09-12 21:10:33 -0500

    fix: restore indexed list clip import

--- lib/src/main/java/org/oneui/compose/components/list/OneUiIndexedList.kt
+++ lib/src/main/java/org/oneui/compose/components/list/OneUiIndexedList.kt
@@ -32,6 +32,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalLayoutDirection
