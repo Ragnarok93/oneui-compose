77e7cf1280405e81593003ed217040a32459e84d
Ragnarok93 <e_fontenot1@yahoo.com>
2026-09-22 05:56:48 -0500

    fix: compile picker color dialog

--- lib/src/main/java/org/oneui/compose/picker/color/SimpleColorPicker.kt
+++ lib/src/main/java/org/oneui/compose/picker/color/SimpleColorPicker.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -85,7 +86,7 @@ fun SimpleColorPickerDialog(
     AlertDialog(
         modifier = modifier,
         onDismissRequest = onDismissRequest,
-        title = title,
+        title = { Text(title) },
         positiveButtonLabel = stringResource(R.string.sesl_picker_done),
         onPositiveButtonClick = {
             onColorSelected(pendingColor)
