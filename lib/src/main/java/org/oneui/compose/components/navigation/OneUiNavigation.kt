1ae60a36f173023962f1cd3aa684f8fbdb721183
Ragnarok93 <e_fontenot1@yahoo.com>
2026-09-22 03:38:38 -0500

    fix: complete navigation state delegate imports

--- lib/src/main/java/org/oneui/compose/components/navigation/OneUiNavigation.kt
+++ lib/src/main/java/org/oneui/compose/components/navigation/OneUiNavigation.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
